b235d6bb35a081f67a4d72d3213a96c972d09f55
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-04-03 12:08:19 -0300

    fix: autoloading

--- config/initializers/simple_form_autocomplete_zipcode.rb
+++ config/initializers/simple_form_autocomplete_zipcode.rb
@@ -1,14 +1,9 @@
 # frozen_string_literal: true
 
-#
-# Uncomment this and change the path if necessary to include your own
-# components.
-# See https://github.com/heartcombo/simple_form#custom-components to know
-# more about custom components.
-Dir[Rails.root.join('app/inputs/**/*.rb')].sort.each { |f| require f }
-#
-# Use this setup block to configure all options available in SimpleForm.
+if defined?(SimpleForm)
+  Dir[Rails.root.join('app/inputs/**/*.rb')].sort.each { |f| require f }
 
-SimpleForm.setup do |config|
-  config.custom_inputs_namespaces << 'AutocompleteZipcode'
+  SimpleForm.setup do |config|
+    config.custom_inputs_namespaces << 'AutocompleteZipcode'
+  end
 end
--- lib/autocomplete_zipcode.rb
+++ lib/autocomplete_zipcode.rb
@@ -4,13 +4,5 @@ require 'autocomplete_zipcode/version'
 require 'rails'
 
 module AutocompleteZipcode
-  if defined?(SimpleForm)
-    autoload :ZipcodeInput, 'app/inputs/autocomplete_zipcode/zipcode_input'
-    autoload :StreetInput, 'app/inputs/autocomplete_zipcode/street_input'
-    autoload :NeighborhoodInput, 'app/inputs/autocomplete_zipcode/neighborhood_input'
-    autoload :CityInput, 'app/inputs/autocomplete_zipcode/city_input'
-    autoload :StateInput, 'app/inputs/autocomplete_zipcode/state_input'
-    autoload :StateSelectInput, 'app/inputs/autocomplete_zipcode/state_select_input'
-    autoload :IbgeInput, 'app/inputs/autocomplete_zipcode/ibge_input'
-  end
+  class Engine < ::Rails::Engine; end
 end
